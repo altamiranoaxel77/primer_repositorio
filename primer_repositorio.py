515c6e6403a1791fd4c9aed1eebf98aec4409d68
altamiranoaxel77 <altamirano.programador@gmail.com>
2023-03-05 19:36:33 -0300

    Add a heading to primer_repositorio.py

---
+++ primer_repositorio.py
@@ -0,0 +1,14 @@
+#suma
+
+def suma(numero1, numero2):
+    return numero1 + numero2
+num1 = float(input("ingresar el numero 1: "))
+num2 = float(input("ingresar el numero 2: "))
+
+print(f'El resultado de la suma es" {suma(num1, num2)}')
+
+#resta
+
+#multiplicacion
+
+#division
