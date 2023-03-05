85c418b235f2290513f94fd0608d686099193e27
altamiranoaxel77 <altamirano.programador@gmail.com>
2023-03-05 20:03:50 -0300

    Add py code to primer_repositorio.py

--- primer_repositorio.py
+++ primer_repositorio.py
@@ -1,13 +1,15 @@
 #suma
 
 def suma(numero1, numero2):
-    return numero1 + numero2
-num1 = float(input("ingresar el numero 1: "))
-num2 = float(input("ingresar el numero 2: "))
-
-print(f'El resultado de la suma es" {suma(num1, num2)}')
+   return numero1 + numero2
 
 #resta
+
+def resta(*args):
+    resultado = args[0]
+    for num in args[1 : ]:
+        resultado = resultado - num
+    return resultado
 
 #multiplicacion
 
